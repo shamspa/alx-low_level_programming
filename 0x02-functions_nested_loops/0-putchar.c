530b2c1e2d9b4c646599379918103723d8265f46
shamspa <wakanyibeth@gmail.com>
2022-06-07 12:56:22 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,17 @@
 /**
  *main - main function
  *Description: program that prints _putchar
+ *Return: 0
  */
 int main(void)
 {
-	_putchar("_putchar");
-	return (0);
+    char c[8] = "_putchar";
+    int i = 0;
+    while (i < 8)
+    {
+    _putchar(c[i]);
+    i++;
+    }
+    _putchar('\n');
+    return (0);
 }
